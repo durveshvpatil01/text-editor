f9edb01d0334006dda1490a810c1ad09595dc4bc
Durvesh Patil <71640344+durveshvpatil01@users.noreply.github.com>
2023-12-08 16:41:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,21 @@
-/**
- * TODO: Update the text in the "Formatted Text" section as a user types in the textarea
- * TODO TOGETHER: Add a .bold, .italic classes to "Formatted Text" when the appropriate button is clicked
- * TODO: Add an .underline class to "Formatted Text" when Underline button is clicked
- * TODO: Toggle the align style for "Formatted Text" when the appropriate button is clicked
- */
 
-/**
- * Update the output text as a user types in the textarea
- * HINT: Use the onkeydown function inside HTML
- */
-/**
- * Update the output text as you type in the textarea
- */
  function updateText(){
   let text = document.getElementById("text-input").value;
   document.getElementById('text-output').innerText = text;
 }
 
-/**
- * Toggle the bold class for the output text
- */
 function makeBold(elem){
   elem.classList.toggle('active');
   document.getElementById('text-output').classList.toggle('bold');
 }
 
-/**
- * Toggle the italic class for the output text
- */
+
 function makeItalic(elem){
   elem.classList.toggle('active');
   document.getElementById('text-output').classList.toggle('italic');
 }
 
-/**
- * Toggle the underline class for the output text
- * HINT: Toggle the .active class for the button
- * HINT: Use the classList property
- * HINT: Use contains, remove, and add functions
- */
+
 function makeUnderline(elem) {
   //CODE GOES HERE
   elem.classList.toggle('active');
@@ -50,15 +27,9 @@ function makeUnderline(elem) {
   }
 }
 
-/**
- * Toggle the style textAlign attribute
- * Toggle the active state for the align butttons
- * HINT: Use the style property of the element
- * HINT: Make sure to untoggle the active state for all other align buttons
- */
 
 function alignText(elem, alignType) {
-  // CODE GOES HERE
+
   document.getElementById('text-output').style.textAlign = alignType;
   let alignButtons = document.getElementsByClassName('align');
   for(let i = 0; i < alignButtons.length; i++ ){
